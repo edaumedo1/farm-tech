58efc38ac321e8f20c7276730a9f854d7703fb4f
Kim Myeongjin <myungjin2009@naver.com>
2022-01-17 17:27:58 +0900

    통신예제용 코드 업로드

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.use(express.json()); 
+app.use(express.urlencoded( {extended : false } ));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+//통신예제용 간단한 코드. 무조건 true 반환
+app.get('/api/memberlist', function (req,res) {
+    console.log(req.body);
+    return res.json({success:true});
+})
+
+app.listen(port, () => console.log(`서버가 켜졌습니다. 포트번호는 ${port} 입니다.2`));
