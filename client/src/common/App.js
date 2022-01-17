30045def27f07d29f10bba0b4d819fc0bac6ceb0
김덕주 <edaumedo1@naver.com>
2022-01-17 15:50:52 +0900

    Link to test

--- client/src/common/App.js
+++ client/src/common/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Signup from "../pages/Signup/Signup";
-import Home from "../pages/Home/Home"
+import Home from "../pages/Home/Home";
+import Test from "../pages/Test/Test";
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
+        <Route path="/Test" element={<Test />}></Route>
       </Routes>
       {/* <Routes>
         <Route path="/" element={<Home />}>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <div>
-            <Link to="/signup">회원가입</Link>
+            <Link to="/Signup">회원가입</Link>
+            <Link to="/Test">테스트</Link>
         </div>
     )
 }
