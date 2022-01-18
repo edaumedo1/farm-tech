b9c6480b3f26cdbd3d830f6616f7df7e20ce7624
gunhyung <one0374@naver.com>
2022-01-18 11:22:05 +0900

    modify input location in signup page

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -12,18 +12,9 @@ function Signup() {
         <Logo512 src={farmlogo} alt="React" />
       </LogoSignup>
       <Form>
-        {/* 회원가입 개인 정보 입력 */}
-        <Input type="email" placeholder="이메일"></Input>
-        <Input type="text" placeholder="이름"></Input>
-        <Input placeholder="별명(10자 이내)"></Input>
-        <Input type="password" placeholder="비밀번호"></Input>
-        <Input type="password" placeholder="비밀번호 확인"></Input>
-        <Input placeholder="생년월일(8자리)"></Input>
-        <Input placeholder="전화번호(' - ' 제외)"></Input>
-      </Form>
-      <Form>
         {/* 인증번호 입력 */}
         <Row>
+          <Input type="email" placeholder="이메일"></Input>
           <Input
             placeholder="인증번호 6자리"
             style={{ width: "9.5em" }}
@@ -34,12 +25,22 @@ function Signup() {
         </Row>
       </Form>
       <Form>
+        {/* 회원가입 개인 정보 입력 */}
+
+        <Input type="text" placeholder="이름"></Input>
+        <Input placeholder="별명(10자 이내)"></Input>
+        <Input type="password" placeholder="비밀번호"></Input>
+        <Input type="password" placeholder="비밀번호 확인"></Input>
+        <Input placeholder="생년월일(8자리)"></Input>
+        <Input placeholder="전화번호(' - ' 제외)"></Input>
+      </Form>
+      <Form>
         {/* 회원가입 완료 취소 버튼 */}
         <Row style={{ margin: "1em 0" }}>
           <Button
             type="button"
             style={{ width: "4.5em" }}
-            onClick={useMovePage(-2)}
+            onClick={useMovePage(-1)}
           >
             취소
           </Button>
@@ -67,7 +68,6 @@ const Button = styled.button`
   width: 6.5em;
   height: 40px;
   margin: 10px 0;
-  // padding: 1px 15px;
   border-radius: 10px;
   border-style: none;
   font-size: 16px;
