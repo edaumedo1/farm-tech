71003ee12e60d7189c715b8950bd5517068cb498
Kim Myeongjin <myungjin2009@naver.com>
2022-01-20 15:21:15 +0900

    회원가입 기능 업데이트

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({    //회원가입 스키마()
     email: String,
     name: String,
     nickname: String,
     password: String,
     birth_day: Date,
-    phone_number: String,
-    auth_number: String
+    phone_number: String
 })
-
 userSchema.pre('save', function() {
 
 })
 
+const emailAuthenticationSchema = new mongoose.Schema({
+    email: String,
+    auth_number: String,
+    createdAt: { type: Date, expires: 600, default: Date.now }
+})
+
+
+
 const User = mongoose.model("user", userSchema);
+const emailAuthentication = mongoose.model("temp_emailAuthentication", emailAuthenticationSchema);
 
 exports.userModel = User;
+exports.emailAuthModel = emailAuthentication;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const Mail = require('../middleware/mail');
 const userSchema = require('../models/User');
+
+const emailAuth_DB = mongoose.model('temp_emailAuthentication');    //temp_emailAuthentication DB
+const user_DB = mongoose.model('user');                             //users DB
 
 
 router.get('/',function(req,res,next){
@@ -10,22 +14,63 @@ router.get('/',function(req,res,next){
 
 
 router.post('/register', function(req,res) {
-    const register_schema = new userSchema.userModel({
-        email: req.body.email,
-        name: req.body.name,
-        nickname: req.body.nickname,
-        password: req.body.password,
-        birth_day: req.body.birth_day,
-        phone_number: req.body.phone_number,
-        auth_number: req.body.auth_number
+    const register_schema = new userSchema.userModel(req.body);
+    
+    
+
+    emailAuth_DB.findOne({email:register_schema.email}, function(err,result) {
+        if(err) {
+            res.status(401).json({success:false, why:err})
+        } else if (result == null) {
+            res.status(401).json({success:false, why:"Authentication Time Expired or Email mismatch. 이메일 인증 시간이 초과되었거나, 인증된 이메일과 가입하는 이메일이 틀림."})
+        } else {
+            if(req.body.auth_number.trim() === result.auth_number) {   //인증번호 일치하는지 확인
+                register_schema.save(function(err) {                 //users DB에 회원정보 저장, 회원가입 성공
+                    if(err){
+                        console.log(err);
+                        res.status(401).json({success:false, why:err})
+                    } else {
+                        res.status(200).json({success:true});
+                        emailAuth_DB.deleteMany({email:register_schema.email}, function(err){
+                            if(err) console.log(err);
+                        });
+                    }
+                })
+            } else {
+                res.status(401).json({success:false, why:"Auth_Number mismatch. 인증번호 불일치"})
+            }
+        }
     })
-    console.log(register_schema);
-    res.status(200).json({success:true, contents:register_schema});
 })
 
 router.post('/email', function(req,res){
-    const email_url = req.body.email;
+    const email_url = req.body.email.trim();
     const num = Math.floor(Math.random()*(999999-100000+1)) + 100000; //인증번호 (임시)
+    //const emailAuth_DB = mongoose.model("temp_emailAuthentication");
+    const emailAuth_schema = new userSchema.emailAuthModel({
+        email: email_url,
+        auth_number: num
+    });
+
+    function deleteData (callback) {
+        emailAuth_DB.deleteMany ({email:email_url}, function(err) {
+            if(err) console.log(err);
+            callback();
+        })
+    }
+
+    user_DB.findOne({email:email_url}, function(err,result) {
+        if(err) console.log(err);
+        else if(result != null) {
+            res.status(401).json({success:false, why:"Email already exists. 이미 가입된 이메일"})
+        } else {
+            deleteData(()=> {
+                emailAuth_schema.save(function(err) {
+                    if(err) res.status(500).json({success:false, why:err});
+                });
+            })
+        }
+    })
 
     Mail.sendEmail(
         email_url,
@@ -37,5 +82,11 @@ router.post('/email', function(req,res){
         }
     )
 })
+
+/* 인증번호 따로 처리하는 부분
+router.post('/auth_number',function(req,res) {
+    const received_number = req.body.auth_number;
+})
+*/
 
 module.exports = router;
