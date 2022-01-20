c3701aa5ed058285263ef22f16fa31bdbf41e417
Kim Myeongjin <myungjin2009@naver.com>
2022-01-20 17:05:18 +0900

    이메일 찾기 기능 업로드

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 5;
 
-const userSchema = new mongoose.Schema({    //회원가입 스키마()
+const userSchema = new mongoose.Schema({    //회원가입 스키마
     email: String,
     name: String,
-    nickname: String,
+    nickname: {type:String, unique:1},
     password: String,
     birth_day: Date,
-    phone_number: String
+    phone_number: String,
+    qualification_no: {type: String, default: 0}
 })
-userSchema.pre('save', function() {
-
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')) { //json중 비밀번호가 변경될 때에만
+        //비밀번호를 암호화시킨다.
+        bcrypt.genSalt(saltRounds, function(err,salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err,hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+         next()
+    }
 })
 
 const emailAuthenticationSchema = new mongoose.Schema({
--- server/routes/user.js
+++ server/routes/user.js
@@ -22,15 +22,19 @@ router.post('/register', function(req,res) {
             res.status(401).json({success:false, why:"Authentication Time Expired or Email mismatch. 이메일 인증 시간이 초과되었거나, 인증된 이메일과 가입하는 이메일이 틀림."})
         } else {
             if(req.body.auth_number.trim() === result.auth_number) {   //인증번호 일치하는지 확인
-                register_schema.save(function(err) {                 //users DB에 회원정보 저장, 회원가입 성공
+                register_schema.save(function(err) {                 //users DB에 회원정보 저장, pre hook으로 bcrypt 암호화(models-User.js) 회원가입 성공
                     if(err){
-                        console.log(err);
-                        res.status(401).json({success:false, why:err})
+                        if(err.keyPattern.nickname == 1) {
+                            res.status(401).json({success:false, why:"nickName exists. 닉네임 중복됨"})
+                        } else {
+                            console.log(err);
+                            res.status(401).json({success:false, why:err})
+                        }
                     } else {
                         res.status(200).json({success:true});
-                        emailAuth_DB.deleteMany({email:register_schema.email}, function(err){
-                            if(err) console.log(err);
-                        });
+                        // emailAuth_DB.deleteMany({email:register_schema.email}, function(err){
+                        //     if(err) console.log(err);
+                        // }); 굳이 여기서 안지워줘도, expires 함수에 의해 600초 후에 자동 말소됨(model-User.js-emailAuthenticationSchema) (불필요한 세션 줄이기)
                     }
                 })
             } else {
@@ -42,7 +46,7 @@ router.post('/register', function(req,res) {
 
 router.post('/email', function(req,res){
     const email_url = req.body.email.trim();
-    const num = Math.floor(Math.random()*(999999-100000+1)) + 100000; //인증번호 (임시)
+    const num = Math.floor(Math.random()*(999999-100000+1)) + 100000; //인증번호 생성기
     //const emailAuth_DB = mongoose.model("temp_emailAuthentication");
     const emailAuth_schema = new userSchema.emailAuthModel({
         email: email_url,
@@ -81,8 +85,21 @@ router.post('/email', function(req,res){
             })
         }
     })
-
 })
+
+router.post('/find_email', function(req,res) {
+    const birthday = new Date(req.body.birth_day);
+    user_DB.findOne({name:req.body.name, birth_day:birthday, phone_number:req.body.phone_number}, function(err,result) {
+        if(err) {
+            res.status(500).json({success:false, why:err})
+        } else if(result == null) {
+            res.status(401).json({success:false, why:"user not exists. 일치하는 회원정보 없음"})
+        } else {
+            res.status(200).json({success:true, email:result.email});
+        }
+    })
+})
+
 
 /* 인증번호 따로 처리하는 부분
 router.post('/auth_number',function(req,res) {
